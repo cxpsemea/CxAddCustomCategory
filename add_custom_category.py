13805ff9765c6a4d369c73f858da72be14dcdc47
Miguel Freitas <13312380+miguelfreitas93@users.noreply.github.com>
2020-01-08 11:44:20 +0000

    improve test coverage

--- add_custom_category.py
+++ add_custom_category.py
@@ -1,9 +1,12 @@
 __author__ = 'miguel.freitas@checkmarx.com'
 
+import sys
 import argparse
 import pyodbc
 import json
 import array
+
+DB = "CxDB"
 
 
 def is_str(string):
@@ -42,7 +45,7 @@ def connect_to_db(server, user, password, database):
                 'DRIVER={SQL Server};SERVER=' + server +
                 ';DATABASE=' + database +
                 ';UID=' + user +
-                ';PWD=' + password)
+                ';PWD=' + password, timeout=3)
             print("Connection to", database, "success")
             return conn
         except pyodbc.OperationalError or pyodbc.InterfaceError as error:
@@ -57,7 +60,8 @@ def get_category_type_id_by_name(conn, category_type_name):
         cursor = conn.cursor()
         category_type_id = -1
         cursor.execute(
-            "SELECT id,Typename FROM dbo.CategoriesTypes WHERE TypeName=?", category_type_name)
+            "SELECT id,Typename FROM dbo.CategoriesTypes WHERE TypeName=?",
+            category_type_name)
         rows = cursor.fetchall()
         if len(rows) > 0:
             for row in rows:
@@ -74,7 +78,8 @@ def add_category_type_by_name(conn, category_type_name):
     if is_conn(conn) and is_str(category_type_name):
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO dbo.CategoriesTypes (Typename) VALUES(?)", category_type_name)
+            "INSERT INTO dbo.CategoriesTypes (Typename) VALUES(?)",
+            category_type_name)
         conn.commit()
         return True
     else:
@@ -106,7 +111,8 @@ def delete_categories_by_category_type_id(conn, category_type_id):
     if is_conn(conn) and is_int(category_type_id):
         cursor = conn.cursor()
         cursor.execute(
-            "DELETE FROM dbo.Categories WHERE CategoryType=?", category_type_id)
+            "DELETE FROM dbo.Categories WHERE CategoryType=?",
+            category_type_id)
         conn.commit()
     else:
         raise AttributeError(
@@ -117,7 +123,8 @@ def delete_categories_for_queries_by_category_type_id(conn, category_type_id):
     if is_conn(conn) and is_int(category_type_id):
         cursor = conn.cursor()
         cursor.execute(
-            "DELETE FROM dbo.CategoryForQuery WHERE CategoryId IN (SELECT id FROM dbo.Categories WHERE CategoryType=?)",
+            "DELETE FROM dbo.CategoryForQuery WHERE CategoryId \
+                IN (SELECT id FROM dbo.Categories WHERE CategoryType=?)",
             category_type_id)
         conn.commit()
     else:
@@ -139,47 +146,54 @@ def clean_old_data(conn, category_type_id):
 def add_category(conn, category_name, category_type_id):
     if is_conn(conn) and is_str(category_name) and is_int(category_type_id):
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO dbo.Categories (CategoryName,CategoryType) VALUES(?,?)",
-                       (category_name, category_type_id))
+        cursor.execute("INSERT INTO dbo.Categories (CategoryName,CategoryType) \
+            VALUES(?,?)", (category_name, category_type_id))
         conn.commit()
         return True
     else:
         raise AttributeError(
-            "Connection object or Category Name or Category Type ID was not provided")
+            "Connection object or Category Name or Category Type ID \
+                was not provided")
 
 
 def get_category_id(conn, category_name, category_type_id):
     if is_conn(conn) and is_str(category_name) and is_int(category_type_id):
         cursor = conn.cursor()
-        cursor.execute("SELECT Id FROM dbo.Categories WHERE CategoryName=? AND CategoryType=?",
+        cursor.execute("SELECT Id FROM dbo.Categories WHERE \
+            CategoryName=? AND CategoryType=?",
                        (category_name, category_type_id))
         return cursor.fetchall()[0][0]
     else:
         raise AttributeError(
-            "Connection object or Category Name or Category Type ID was not provided")
+            "Connection object or Category Name or Category Type ID \
+                was not provided")
 
 
 def add_category_for_query(conn, category_id, query_id):
     if is_conn(conn) and is_int(category_id) and is_int(query_id):
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO dbo.CategoryForQuery (QueryId,CategoryId) VALUES(?,?)", (query_id, category_id))
+            "INSERT INTO dbo.CategoryForQuery (QueryId,CategoryId) \
+                VALUES(?,?)", (query_id, category_id))
         conn.commit()
         return True
     else:
         raise AttributeError(
-            "Connection object or Category ID or Query ID was not provided")
+            "Connection object or Category ID or Query ID \
+                was not provided")
 
 
 def update_category_for_query(conn, category_id, query_id):
     if is_conn(conn) and is_int(category_id) and is_int(query_id):
         cursor = conn.cursor()
         try:
-            cursor.execute("SELECT * FROM dbo.CategoryForQuery WHERE CategoryId=? AND QueryId=?",
+            cursor.execute("SELECT * FROM dbo.CategoryForQuery WHERE \
+                CategoryId=? AND QueryId=?",
                            (category_id, query_id))
             rows = cursor.fetchall()
             if len(rows[0]) == 0:
-                cursor.execute("INSERT INTO dbo.CategoryForQuery (QueryId,CategoryId) VALUES(?,?)",
+                cursor.execute("INSERT INTO dbo.CategoryForQuery \
+                    (QueryId,CategoryId) VALUES(?,?)",
                                (query_id, category_id))
                 conn.commit()
             return True
@@ -190,16 +204,20 @@ def update_category_for_query(conn, category_id, query_id):
             "Connection object or Category ID or Query ID was not provided")
 
 
-def get_categories_by_category_type_id_and_name(conn, category_name, category_type_id):
+def get_categories_by_category_type_id_and_name(conn,
+                                                category_name,
+                                                category_type_id):
     if is_conn(conn) and is_int(category_type_id) and is_str(category_name):
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM dbo.Categories WHERE CategoryName=? AND CategoryType=?", category_name,
-                       category_type_id)
+        cursor.execute("SELECT * FROM dbo.Categories WHERE \
+            CategoryName=? AND CategoryType=?",
+                       category_name, category_type_id)
         rows = cursor.fetchall()
         return rows
     else:
         raise AttributeError(
-            "Connection object or Category ID or Query ID was not provided")
+            "Connection object or Category ID or Query ID \
+                was not provided")
 
 
 def insert_new_categories(conn, category_type_id, group):
@@ -214,7 +232,8 @@ def insert_new_categories(conn, category_type_id, group):
             return category_id
     else:
         raise AttributeError(
-            "Connection object or Category Type ID was not provided")
+            "Connection object or Category Type ID \
+                was not provided")
 
 
 def get_queries(conn, query_ids_list):
@@ -236,7 +255,8 @@ def get_categories_ids_by_category_type(conn, category_type_id):
     if is_conn(conn) and is_int(category_type_id):
         cursor = conn.cursor()
         cursor.execute(
-            "SELECT [Id] FROM dbo.Categories where CategoryType=?", category_type_id)
+            "SELECT [Id] FROM dbo.Categories where CategoryType=?",
+            category_type_id)
         rows = cursor.fetchall()
         arr = array.array('i')
         for row in rows:
@@ -258,7 +278,8 @@ def insert_queries(conn, category_id, queries):
         percentage = round((i * 100) / len(queries), 0)
         if is_deprecated == 0:
             print("Inserting Query", query_id, "...", percentage, "%")
-            cursor.execute("INSERT INTO dbo.CategoryForQuery (QueryId,CategoryId) VALUES(?,?)",
+            cursor.execute("INSERT INTO dbo.CategoryForQuery \
+                (QueryId,CategoryId) VALUES(?,?)",
                            (query_id, category_id))
             conn.commit()
         else:
@@ -279,58 +300,74 @@ def update_queries(conn, category_id, group, queries):
         i = i + 1
 
 
-def get_args():
-    args_parser = argparse.ArgumentParser(
-        description='Add Custom Category to CxDB')
-    args_parser.add_argument(
-        '-dbu', '--dbuser', help='Checkmarx MSSQL DB Username', required=True)
-    args_parser.add_argument('-dbp', '--dbpassword',
-                             help='Checkmarx MSSQL DB Password', required=True)
-    args_parser.add_argument('-dbs', '--dbserver',
-                             help='Checkmarx MSSQL DB Server URL', required=True)
-    args_parser.add_argument('-fg', '--file_groups',
-                             help='Categories and Queries Mapping File', required=True)
-    return args_parser.parse_args()
+def get_args(args):
+    if isinstance(args, list) and len(args) > 0:
+        args_parser = argparse.ArgumentParser(
+            description='Add Custom Category to CxDB')
+        args_parser.add_argument(
+            '-dbu', '--dbuser', help='Checkmarx MSSQL DB Username',
+            required=True)
+        args_parser.add_argument('-dbp', '--dbpassword',
+                                 help='Checkmarx MSSQL DB Password',
+                                 required=True)
+        args_parser.add_argument('-dbs', '--dbserver',
+                                 help='Checkmarx MSSQL DB Server URL',
+                                 required=True)
+        args_parser.add_argument('-fg', '--file_groups',
+                                 help='Categories and Queries Mapping File',
+                                 required=True)
+        return args_parser.parse_args(args)
+    else:
+        raise AttributeError("args should be a non-empty array")
 
 
 def main(args):
-    file_groups = args.file_groups
-    if is_str(file_groups):
-        file_content = read_file(file_groups)
-        category = file_content["category"]
-        category_name = category["name"]
-        groups = category["groups"]
+    if args is not None and hasattr(args, "file_groups"):
+        file_groups = args.file_groups
+        if is_str(file_groups):
+            file_content = read_file(file_groups)
+            category = file_content["category"]
+            category_name = category["name"]
+            groups = category["groups"]
+            if hasattr(args, "dbserver") and \
+                    hasattr(args, "dbuser") and \
+                    hasattr(args, "dbpassword"):
+                db_server = args.dbserver
+                db_user = args.dbuser
+                db_pwd = args.dbpassword
+                if is_str(db_server) and \
+                        is_str(db_user) and \
+                        is_str(db_pwd):
 
-        db_server = args.dbserver
-        db_user = args.dbuser
-        db_pwd = args.dbpassword
-        database = "CxDB"
-        if is_str(db_server) and \
-                is_str(db_user) and \
-                is_str(db_pwd):
+                    conn = connect_to_db(db_server, db_user, db_pwd, DB)
 
-            conn = connect_to_db(db_server, db_user, db_pwd, database)
-
-            if is_conn(conn):
-                category_type_id = check_category_type_by_name(
-                    conn, category_name)
-                clean_old_data(conn, category_type_id)
-                for group in groups:
-                    category_id = insert_new_categories(
-                        conn, category_type_id, group)
-                    queries = get_queries(conn, group["queryIds"])
-                    print(group["name"], ":", len(queries),
-                          "queries to change")
-                    insert_queries(conn, category_id, queries)
-                    update_queries(conn, category_id, group, queries)
+                    if is_conn(conn):
+                        category_type_id = check_category_type_by_name(
+                            conn, category_name)
+                        clean_old_data(conn, category_type_id)
+                        for group in groups:
+                            category_id = insert_new_categories(
+                                conn, category_type_id, group)
+                            queries = get_queries(conn, group["queryIds"])
+                            print(group["name"], ":", len(queries),
+                                  "queries to change")
+                            insert_queries(conn, category_id, queries)
+                            update_queries(conn, category_id, group, queries)
+                    else:
+                        raise Exception("Cannot connect to Database")
+                else:
+                    raise Exception(
+                        "db_server | db_user | db_pwd \
+                            are not valid strings")
             else:
-                raise Exception("Cannot connect to Database")
+                raise Exception(
+                    "db_server | db_user | db_pwd \
+                        was not provided as an argument")
         else:
-            raise Exception(
-                "db_server | db_user | db_pwd was not provided as an argument")
+            raise TypeError("file_groups is not a string")
     else:
-        raise Exception("file_groups was not provided as an argument")
+        raise AttributeError("args does not has file_groups as attribute")
 
 
 if __name__ == "__main__":
-    main(get_args())
+    main(get_args(sys.argv[1:]))
--- test_add_custom_category.py
+++ test_add_custom_category.py
@@ -1,8 +1,22 @@
 __author__ = 'miguel.freitas@checkmarx.com'
 
-from add_custom_category import is_str, is_int, is_conn, read_file
+from add_custom_category import is_str
+from add_custom_category import is_int
+from add_custom_category import is_conn
+from add_custom_category import read_file
+from add_custom_category import connect_to_db
+from add_custom_category import get_category_type_id_by_name
+
+from add_custom_category import get_args
+from add_custom_category import main
+
+from collections import namedtuple
 import pytest
 FILE = "groups.json"
+DBU = "test"
+DBP = "test"
+DBS = "TESTSERVER"
+DBD = "Database"
 
 
 def test_is_str():
@@ -45,12 +59,12 @@ def test_is_conn():
     assert is_conn({}) is False
     assert is_conn("") is False
     assert is_conn("123") is False
-    assert is_conn("123") is False
     # Missing pyodbc.Connection test
 
 
 def test_read_file():
-    with pytest.raises(AttributeError, match="No Filename provided"):
+    with pytest.raises(AttributeError,
+                       match="No Filename provided"):
         assert read_file(None)
         assert read_file(True)
         assert read_file(False)
@@ -61,9 +75,130 @@ def test_read_file():
         assert read_file([])
         assert read_file({})
         assert read_file("")
-    with pytest.raises(AttributeError, match="File should have \".json\" extension"):
+    with pytest.raises(AttributeError,
+                       match="File should have \".json\" extension"):
         assert read_file("test")
-    with pytest.raises(FileNotFoundError, match="File Not Found"):
+    with pytest.raises(FileNotFoundError,
+                       match="File Not Found"):
         assert read_file("test.json")
     json = read_file(FILE)
     assert len(json) > 0
+
+
+def test_connect_to_db():
+    with pytest.raises(TypeError):
+        assert connect_to_db()
+    with pytest.raises(AttributeError,
+                       match="server ! user | password | database \
+                           were not provided"):
+        assert connect_to_db("", "", "", "")
+    with pytest.raises(ConnectionError):
+        assert connect_to_db(DBS, DBU, DBP, DBD)
+
+
+def get_category_type_id_by_name():
+    assert get_category_type_id_by_name("", "")
+    with pytest.raises(AttributeError,
+                       match="Connection object or Category Name \
+                           was not provided"):
+        assert get_category_type_id_by_name(None, None)
+
+
+def test_get_args():
+    with pytest.raises(AttributeError,
+                       match="args should be a non-empty array"):
+        assert get_args(None)
+        assert get_args(True)
+        assert get_args(False)
+        assert get_args(1)
+        assert get_args(0)
+        assert get_args(-1)
+        assert get_args(1.1)
+        assert get_args([])
+        assert get_args({})
+        assert get_args("")
+        assert get_args("False")
+    args = get_args(["-dbu", DBU, "-dbs", DBS, "-dbp", DBP, "-fg", FILE])
+    assert hasattr(args, "file_groups") and \
+        hasattr(args, "dbserver") and \
+        hasattr(args, "dbuser") and \
+        hasattr(args, "dbpassword")
+    assert args.dbuser == DBU and args.dbpassword == DBP
+    assert args.dbserver == DBS and args.file_groups == FILE
+
+
+def test_main():
+    with pytest.raises(AttributeError,
+                       match="args does not has file_groups as attribute"):
+        assert main(None)
+        assert main(True)
+        assert main(False)
+        assert main(1)
+        assert main(0)
+        assert main(-1)
+        assert main(1.1)
+        assert main([])
+        assert main({})
+        assert main("")
+        assert main("123")
+    a = {"file_groups": None}
+    args = namedtuple("Arguments", a.keys())(*a.values())
+    assert hasattr(args, "file_groups")
+    with pytest.raises(TypeError,
+                       match="file_groups is not a string"):
+        assert main(args)
+        args.file_groups = True
+        assert main(args)
+        args.file_groups = False
+        assert main(args)
+        args.file_groups = 1
+        assert main(args)
+        args.file_groups = 0
+        assert main(args)
+        args.file_groups = -1
+        assert main(args)
+        args.file_groups = 1.1
+        assert main(args)
+        args.file_groups = []
+        assert main(args)
+        args.file_groups = {}
+        assert main(args)
+        args.file_groups = ""
+        assert main(args)
+    a = {"file_groups": "123"}
+    args = namedtuple("Arguments", a.keys())(*a.values())
+    assert hasattr(args, "file_groups")
+    with pytest.raises(AttributeError,
+                       match='File should have \".json\" extension'):
+        assert main(args)
+        args.file_groups = "groups.txt"
+        assert main(args)
+    a = {"file_groups": FILE}
+    args = namedtuple("Arguments", a.keys())(*a.values())
+    assert hasattr(args, "file_groups")
+    with pytest.raises(Exception,
+                       match="db_server | db_user | db_pwd \
+                           was not provided as an argument"):
+        assert main(args)
+
+    a = {"file_groups": FILE, "dbserver": "",
+         "dbuser": "", "dbpassword": ""}
+    args = namedtuple("Arguments", a.keys())(*a.values())
+    assert hasattr(args, "file_groups") and \
+        hasattr(args, "dbserver") and \
+        hasattr(args, "dbuser") and \
+        hasattr(args, "dbpassword")
+    with pytest.raises(Exception,
+                       match="db_server | db_user | db_pwd \
+                           are not valid strings"):
+        assert main(args)
+
+    a = {"file_groups": FILE, "dbserver": DBS,
+         "dbuser": DBU, "dbpassword": DBP}
+    args = namedtuple("Arguments", a.keys())(*a.values())
+    assert hasattr(args, "file_groups") and \
+        hasattr(args, "dbserver") and \
+        hasattr(args, "dbuser") and \
+        hasattr(args, "dbpassword")
+    with pytest.raises(ConnectionError):
+        assert main(args)
