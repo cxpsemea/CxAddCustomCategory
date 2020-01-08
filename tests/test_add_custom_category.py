aec7f82acf0d8414da7b4a8e2cfd9759a7adef82
Miguel Freitas <13312380+miguelfreitas93@users.noreply.github.com>
2020-01-08 15:08:09 +0000

    remove imports fix attempt

--- tests/test_add_custom_category.py
+++ tests/test_add_custom_category.py
@@ -1,10 +1,5 @@
 __author__ = 'miguel.freitas@checkmarx.com'
 
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
-
 from add_custom_category import is_str
 from add_custom_category import is_int
 from add_custom_category import is_conn
