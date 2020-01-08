87c588d8cb3339983cac0c004234d5739daeb9f1
Miguel Freitas <13312380+miguelfreitas93@users.noreply.github.com>
2020-01-08 09:22:23 +0000

    add launch and db scripts

---
+++ db/delete_entries.sql
@@ -0,0 +1,3 @@
+DELETE FROM [CxDB].[dbo].[CategoryForQuery] WHERE CategoryId > 71
+DELETE FROM [CxDB].[dbo].[Categories] WHERE Id > 71
+DELETE FROM [CxDB].[dbo].[CategoriesTypes] WHERE Id > 7
---
+++ db/get_tables.sql
@@ -0,0 +1,3 @@
+SELECT * FROM [CxDB].[dbo].[CategoryForQuery] WHERE CategoryId > 71
+SELECT * FROM [CxDB].[dbo].[Categories] WHERE Id > 71
+SELECT * FROM [CxDB].[dbo].[CategoriesTypes] WHERE Id > 7
